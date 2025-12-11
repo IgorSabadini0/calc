58605293bf05d12671aacdb8edc073860594ffa4
Safadini <igorsabadini157@gmail.com>
2025-12-11 16:26:03 -0300

    Atualizando o repositório com as últimas mudanças no código-fonte.

---
+++ js/script.js
@@ -0,0 +1,28 @@
+let digitos = '';
+
+
+function addNumber(number) {
+    digitos += number;
+    document.getElementById("itensScreen").value = digitos; //o valor dos 'itensScreen' é igual ao que for digitado.
+}
+
+function addOp(op) {
+    digitos += op;
+    document.getElementById('itensScreen').value = digitos;
+}
+
+function clearScreen() {
+    digitos = '';
+    document.getElementById('itensScreen').value = digitos;
+}
+
+function delDigito() {
+    digitos = digitos.slice(0, -1);
+    document.getElementById('itensScreen').value = digitos;
+}
+
+function result() {
+    if (op === '&#43;') {
+        let valores = digitos.split('+')
+    }
+}
